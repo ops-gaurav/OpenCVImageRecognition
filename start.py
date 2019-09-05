4ca4e82b286c4b99f9c0a02941012217e0b7712b
gaurav <sharma02gaurav@gmail.com>
2019-09-05 13:29:25 +0530

    adding grayscale to video

--- start.py
+++ start.py
@@ -9,10 +9,10 @@ while(True):
 	ret, frame = cap.read()
 
 	# implement the frame operations here
-	# gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+	gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
 	# display the frame
-	cv2.imshow('frame', frame)
+	cv2.imshow('frame', gray)
 	# cv2.imshow('gray', gray)
 
 	if cv2.waitKey(20) & 0XFF == ord('q'):
